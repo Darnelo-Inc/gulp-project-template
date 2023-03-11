b6435e92200b85e305c7669309d136d11b018304
Pavel Gorbunov <darnelo@inbox.ru>
2023-03-11 17:21:09 +0300

    add webmanifest xml image formats

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 "use strict"
 
-
-const {src, dest} = require("gulp")
+const { src, dest } = require("gulp")
 const gulp = require("gulp")
 const browsersync = require("browser-sync").create()
 const del = require("del")
@@ -18,133 +17,132 @@ const removeComments = require("gulp-strip-css-comments")
 const uglify = require("gulp-uglify")
 const panini = require("panini")
 
-
 /* Paths */
 
 let path = {
-    build: {
-        html: "dist/",
-        js: "dist/assets/js/",
-        css: "dist/assets/css/",
-        images: "dist/assets/img/"
-    },
-    
-    src: {
-        html: "src/*.html",
-        js: "src/assets/js/*.js",
-        css: "src/assets/less/style.less",
-        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
-    },
-    
-    watch: {
-        html: "src/**/*.html",
-        js: "src/assets/js/**/*.js",
-        css: "src/assets/less/**/*.less",
-        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
-    },
-    
-    clean: "./dist"
-}
+  build: {
+    html: "dist/",
+    js: "dist/assets/js/",
+    css: "dist/assets/css/",
+    images: "dist/assets/img/",
+  },
 
+  src: {
+    html: "src/*.html",
+    js: "src/assets/js/*.js",
+    css: "src/assets/less/style.less",
+    images: "src/assets/img/**/*.{jpg,png,svg,gif,ico,webmanifest,xml}",
+  },
+
+  watch: {
+    html: "src/**/*.html",
+    js: "src/assets/js/**/*.js",
+    css: "src/assets/less/**/*.less",
+    images: "src/assets/img/**/*.{jpg,png,svg,gif,ico,webmanifest,xml}",
+  },
+
+  clean: "./dist",
+}
 
 /* Tasks */
 
 function browserSync(done) {
-    browsersync.init({
-        server: {
-            baseDir: "./dist/"
-        },
-        port: 3000
-    })
+  browsersync.init({
+    server: {
+      baseDir: "./dist/",
+    },
+    port: 3000,
+  })
 }
-
 
 function browserSyncReload(done) {
-    browsersync.reload()
+  browsersync.reload()
 }
-
 
 function html() {
-    panini.refresh()
-    return src(path.src.html, {base: "src/"})
-        .pipe(plumber())
-        .pipe(panini({
-            root: 'src/',
-            layouts: 'src/templates/layouts/',
-            partials: 'src/templates/partials/',
-            helpers: 'src/templates/helpers/',
-            data: 'src/templates/data/'
-        }))
-        .pipe(dest(path.build.html))
-        .pipe(browsersync.stream())
+  panini.refresh()
+  return src(path.src.html, { base: "src/" })
+    .pipe(plumber())
+    .pipe(
+      panini({
+        root: "src/",
+        layouts: "src/templates/layouts/",
+        partials: "src/templates/partials/",
+        helpers: "src/templates/helpers/",
+        data: "src/templates/data/",
+      })
+    )
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream())
 }
-
 
 function css() {
-    return src(path.src.css, {base: "src/assets/less"})
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(autoprefixer({
-            Browserslist: ['last 8 versions'],
-            cascade: false
-        }))
-        .pipe(cssbeautify())
-        .pipe(dest(path.build.css))
-        .pipe(cssnano({
-            zindex: false,
-            discardComments: {
-                removeAll: true
-            }
-        }))
-        .pipe(removeComments())
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".css"
-        }))
-        .pipe(dest(path.build.css))
-        .pipe(browsersync.stream())
+  return src(path.src.css, { base: "src/assets/less" })
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(
+      autoprefixer({
+        Browserslist: ["last 8 versions"],
+        cascade: false,
+      })
+    )
+    .pipe(cssbeautify())
+    .pipe(dest(path.build.css))
+    .pipe(
+      cssnano({
+        zindex: false,
+        discardComments: {
+          removeAll: true,
+        },
+      })
+    )
+    .pipe(removeComments())
+    .pipe(
+      rename({
+        suffix: ".min",
+        extname: ".css",
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
 }
-
 
 function js() {
-    return src(path.src.js, {base: './src/assets/js/'})
-        .pipe(plumber())
-        .pipe(rigger())
-        .pipe(gulp.dest(path.build.js))
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".js"
-        }))
-        .pipe(dest(path.build.js))
-        .pipe(browsersync.stream())
+  return src(path.src.js, { base: "./src/assets/js/" })
+    .pipe(plumber())
+    .pipe(rigger())
+    .pipe(gulp.dest(path.build.js))
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: ".min",
+        extname: ".js",
+      })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
 }
-
 
 function images() {
-    return src(path.src.images)
-        .pipe(imagemin())
-        .pipe(dest(path.build.images))
-	.pipe(browsersync.stream())
+  return src(path.src.images)
+    .pipe(imagemin())
+    .pipe(dest(path.build.images))
+    .pipe(browsersync.stream())
 }
-
 
 function clean() {
-    return del(path.clean)
+  return del(path.clean)
 }
-
 
 function watchFiles() {
-    gulp.watch([path.watch.html], html)
-    gulp.watch([path.watch.css], css)
-    gulp.watch([path.watch.js], js)
-    gulp.watch([path.watch.images], images)
+  gulp.watch([path.watch.html], html)
+  gulp.watch([path.watch.css], css)
+  gulp.watch([path.watch.js], js)
+  gulp.watch([path.watch.images], images)
 }
-
 
 const build = gulp.series(clean, gulp.parallel(html, css, js, images))
 const watch = gulp.parallel(build, watchFiles, browserSync)
-
 
 /* Exports Tasks */
 
